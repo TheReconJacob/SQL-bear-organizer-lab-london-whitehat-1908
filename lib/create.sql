be11a07d5e1730e1392c7eb66d81459bebac83e3
Jacob Vibert <jvpokeyjake@gmail.com>
2019-08-15 10:25:22 +0000

    Automatically backed up by Learn

--- lib/create.sql
+++ lib/create.sql
@@ -1,10 +1,9 @@
-CREATE TABLE bears (
-    ID int NOT NULL,
-    name,
-    age,
-    gender,
-    color,
-    temperament,
-    alive,
-    
+CREATE TABLE bears
+( id int NOT NULL,
+  name char(50),
+  age int,
+  gender char(50),
+  color char(50),
+  temperament char(50),
+  alive char(50),
 );
