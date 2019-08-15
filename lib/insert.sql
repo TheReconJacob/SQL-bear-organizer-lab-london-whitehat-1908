8ac80b155f6db914757ea9cd2e1f19554271f807
Jacob Vibert <jvpokeyjake@gmail.com>
2019-08-15 10:39:36 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,7 +1,7 @@
 INSERT INTO bears (name)
     VALUES ("Mr. Chocolate");
     VALUES ("Rowdy");
-    VALUES ("Angelo");
+    VALUES ("Tabitha");
     VALUES ("Sheila");
     VALUES ("Bill");
     VALUES ("Joe");
